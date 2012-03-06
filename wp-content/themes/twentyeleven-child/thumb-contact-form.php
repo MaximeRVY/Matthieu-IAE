4935dbc4f615cb5684acf9c005f10c84513803b5
maxime <maxime.raverdy@gmail.com>
2012-03-06 21:28:27 +0100

    Change short-code for contact-form for wordpress on line

--- wp-content/themes/twentyeleven-child/thumb-contact-form.php
+++ wp-content/themes/twentyeleven-child/thumb-contact-form.php
@@ -1,4 +1,4 @@
 <div id="contact-form" class="thumb-part">
   <h3 class="thumb-title">Posez vos Questions</h3>
-  <?php echo do_shortcode('[contact-form-7 id="54" title="Contact form 1"]'); ?>
+  <?php echo do_shortcode('[contact-form-7 id="23" title="home"]'); ?>
 </div>
